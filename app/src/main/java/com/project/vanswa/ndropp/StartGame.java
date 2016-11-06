8f66500d2135a35b98e0d41c2c975c5854f44b06
Vanswa Garbutt <vgarbutt@gmail.com>
2016-11-06 14:54:35 -0600

    added create game view and button launcher

--- app/src/main/java/com/project/vanswa/ndropp/StartGame.java
+++ app/src/main/java/com/project/vanswa/ndropp/StartGame.java
@@ -1,7 +1,9 @@
 package com.project.vanswa.ndropp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartGame extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class StartGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+
+
+
+        //create a view for the create game screen
+        //create a view for the join game screen
+        //link the buttons to intents
+        //link intents to start views
+    }
+
+    public void createGame(View view){
+        Intent intent = new Intent(getApplicationContext(),CreateGame.class);
+        startActivity(intent);
     }
 }
+
